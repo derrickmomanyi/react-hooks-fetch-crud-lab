f82734d3016ee62ae087b665494235866bccd85a
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-01-17 01:37:59 +0300

    Completed Assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 function QuestionForm(props) {
+
+
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -16,15 +18,37 @@ function QuestionForm(props) {
       [event.target.name]: event.target.value,
     });
   }
-
+   
+    
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+
+    //create the question data to be posted
+    const questionData = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4
+
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    }
+
+
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(questionData)
+    });
   }
 
   return (
     <section>
-      <h1>New Question</h1>
+      <h1>Add New Question</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Prompt:
